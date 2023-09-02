fd4e5c5046320e2d0b04b946941f6be809f06374
Bappa Barik <61709328+bappabarik@users.noreply.github.com>
2023-09-02 10:52:33 +0530

    More responsiveness added

--- script.js
+++ script.js
@@ -3,20 +3,24 @@ const title = document.querySelector('.img-title')
 const explanation = document.querySelector('.explanation')
 
 const requestUrl = 'https://api.nasa.gov/planetary/apod?api_key=8JxbEAGtqOqlstuQsg4kQ2ykKDd0w4xddSDmBDzf'
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', requestUrl)
-    xhr.onreadystatechange = function(){
-        console.log(xhr.readyState);
-        if (xhr.readyState === 4) {
+const xhr = new XMLHttpRequest();
+xhr.open('GET', requestUrl)
+xhr.onreadystatechange = function () {
+    // console.log(xhr.readyState);
+    if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
             const data = JSON.parse(this.responseText)
-            // console.log(data.hdurl);
-            // console.log(data.title);
-            // console.log(data.explanation);
             image.innerHTML = `<img
             src="${data.hdurl}"
             alt=""/>`
             title.innerHTML = `${data.title}`
             explanation.innerHTML = `${data.explanation}`
-        }
-    }
-    xhr.send();
+        } else {
+            console.error(`Request failed with status: ${xhr.status}`);
+        };
+    };
+};
+xhr.onerror = function () {
+    document.querySelector('body').innerHTML = 'An error occurred during the request.'
+};
+xhr.send();
